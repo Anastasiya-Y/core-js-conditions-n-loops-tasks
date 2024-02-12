3bdb14c32c6fedd59b51d9ab95e96a06b9f4ef7d
Anastasiya Yacheykina <yacheykina_nastya@mail.ru>
2024-02-13 02:56:59 +0300

    feat: add rotateMatrix

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -425,8 +425,24 @@ function getSpiralMatrix(size) {
  *    [7, 8, 9]         [9, 6, 3]
  *  ]                 ]
  */
-function rotateMatrix(/* matrix */) {
-  throw new Error('Not implemented');
+function rotateMatrix(matrix) {
+  const newMatrix = [];
+  const result = matrix;
+
+  for (let i = 0; i < matrix.length; i += 1) {
+    newMatrix[i] = [];
+    for (let j = 0; j < matrix.length; j += 1) {
+      newMatrix[i][j] = matrix[i][j];
+    }
+  }
+
+  for (let i = 0; i < newMatrix.length; i += 1) {
+    for (let j = 0; j < newMatrix[0].length; j += 1) {
+      result[j][newMatrix.length - i - 1] = newMatrix[i][j];
+    }
+  }
+
+  return result;
 }
 
 /**
